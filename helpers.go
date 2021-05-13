e5ab75d4a85a93fb982ddd7412ba77324dcc6b40
Quinn Guerin <qugu2427@colorao.edu>
2021-05-13 13:55:17 -0600

    fixed image regex

--- helpers.go
+++ helpers.go
@@ -12,8 +12,8 @@ import (
 const (
 	alphaNum     = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234567890"
 	maxImageSize = 500000
-	imageUrlRgx  = `^https://api.crowdreport.com/images/.+$` //^https:\/\/api.crowdreport.com\/[0-9a-zA-z]{1,75}$
-	titleRgx     = `^\S.{13,73}\S$`                          //^.{15,75}$
+	imageUrlRgx  = `^https://api.crowdreport.me/images/.+$` //^https:\/\/api.crowdreport.com\/[0-9a-zA-z]{1,75}$
+	titleRgx     = `^\S.{13,73}\S$`                         //^.{15,75}$
 	tagsRgx      = "^.{1,75}$"
 	tagRgx       = "^\\/?(h[1-6]|p|br|u|strong|em|ul|ol|li|(span|img|iframe)( ?(class=\"[^\"]*\"|style=\"((background-color|color): ?rgb\\( ?[0-9]{1,3}, ?[0-9]{1,3}, ?[0-9]{1,3}\\); ?){1,2}\"|src=\"[^\"]*\"|frameborder=\"[^\"]*\"|allowfullscreen=\"(true|false)\")){0,5}?)$"
 )
