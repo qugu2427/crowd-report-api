ea06d99d7495ec9f7e44d32cd4eec4c212fe4e5c
Quinn Guerin <qugu2427@colorao.edu>
2021-05-14 10:13:58 -0600

    fixed create regex

--- helpers.go
+++ helpers.go
@@ -15,7 +15,7 @@ const (
 	imageUrlRgx  = `^https://api.crowdreport.me/images/.+$` //^https:\/\/api.crowdreport.com\/[0-9a-zA-z]{1,75}$
 	titleRgx     = `^\S.{13,73}\S$`                         //^.{15,75}$
 	tagsRgx      = "^.{1,75}$"
-	tagRgx       = "^\\/?(h[1-6]|p|br|u|strong|em|ul|ol|li|(span|img|iframe)( ?(class=\"[^\"]*\"|style=\"((background-color|color): ?rgb\\( ?[0-9]{1,3}, ?[0-9]{1,3}, ?[0-9]{1,3}\\); ?){1,2}\"|src=\"[^\"]*\"|frameborder=\"[^\"]*\"|allowfullscreen=\"(true|false)\")){0,5}?)$"
+	tagRgx       = `^(\/?)(h[1-6]|p|br|u|strong|em|ul|ol|li|span|img|iframe)(.?((class|src|frameborder|allowfullscreen)="[^";]*"|style="((background-color|color): ?[^":;]*; ?){0,5}")){0,5}$`
 )
 
 var allowedImageMimes = [7]string{"png", "jpg", "jpeg", "gif", "bmp", "jfif", "svg"}
